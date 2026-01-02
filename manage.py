67d70e69e11b9f01e94c9deb941dbaf999b26c92
Shubham Kumar Patel <shubhkrpatel@gmail.com>
2026-01-03 00:37:11 +0530

    Switched to IPv4 instead of IPv6 for email

--- manage.py
+++ manage.py
@@ -2,6 +2,23 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import socket  # <--- 1. Import socket
+
+# --- 2. THIS BLOCK TO FORCE IPV4 ---
+# This forces Django to use IPv4 (Standard Internet) instead of IPv6
+# to prevent Gmail timeouts on Railway.
+try:
+    orig_getaddrinfo = socket.getaddrinfo
+
+    def getaddrinfo_ipv4(host, port, family=0, type=0, proto=0, flags=0):
+        if family == 0:
+            family = socket.AF_INET  # Force IPv4
+        return orig_getaddrinfo(host, port, family, type, proto, flags)
+
+    socket.getaddrinfo = getaddrinfo_ipv4
+except Exception:
+    pass
+#---------------------------------------------------
 
 
 def main():
